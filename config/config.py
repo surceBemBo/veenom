2142e16ece33469ef6c8487ccd85f26b5a353e78
surceBemBo <106420621+surceBemBo@users.noreply.github.com>
2022-08-29 20:16:10 +0200

    Update config.py
    Signed-off-by: surceBemBo <106420621+surceBemBo@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001709259736"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "music bot venom")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "music bot arnop")
 
 # Your User ID.
 OWNER_ID = list(
