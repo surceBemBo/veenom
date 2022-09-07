205a2861694c3ea891c2d71326f3c9f6d2f62ff1
surceBemBo <106420621+surceBemBo@users.noreply.github.com>
2022-09-07 20:34:58 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -64,8 +64,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
-SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/l_YT_l"
+CHANNEL_SUDO = getenv(
+    "CHANNEL_SUDO", "https://t.me/l_YT_l"
 )  # Example:- https://t.me/l_YT_l
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", "https://t.me/l_lpl_l"
