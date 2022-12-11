ebe5e44427c93037d74d8bc8c29418d2e22fb87b
surceBemBo <106420621+surceBemBo@users.noreply.github.com>
2022-12-11 11:50:09 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001463424967"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "music bot toxic ")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "music bot arnop ")
 
 # Your User ID.
 OWNER_ID = list(
