2a0444347afc1a8fef9a189aedf2481639cfecc7
surceBemBo <106420621+surceBemBo@users.noreply.github.com>
2022-12-11 12:03:36 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -44,7 +44,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "music bot arnop ")
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "").split())
+    map(int, getenv("OWNER_ID", "5934060011").split())
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
